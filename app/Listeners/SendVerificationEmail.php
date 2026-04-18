7ab46ee2e5da7d5f5c25357035291222d65b342d
Hagar-Elbakry <161838415+Hagar-Elbakry@users.noreply.github.com>
2026-04-18 11:59:00 +0000

    Fix coding style with Laravel Pint

--- app/Listeners/SendVerificationEmail.php
+++ app/Listeners/SendVerificationEmail.php
@@ -11,7 +11,7 @@ class SendVerificationEmail implements ShouldQueue
 {
     public function handle(UserRegistered $event): void
     {
-        if(filter_var($event->user->contact, FILTER_VALIDATE_EMAIL)) {
+        if (filter_var($event->user->contact, FILTER_VALIDATE_EMAIL)) {
             Mail::to($event->user->contact)->send(new EmailVerificationMail($event->user, $event->otpCode));
         }
 
--- app/Listeners/SendWelcomeEmail.php
+++ app/Listeners/SendWelcomeEmail.php
@@ -9,13 +9,12 @@ use Illuminate\Support\Facades\Mail;
 
 class SendWelcomeEmail implements ShouldQueue
 {
-
     /**
      * Handle the event.
      */
     public function handle(UserRegistered $event): void
     {
-        if(filter_var($event->user->contact, FILTER_VALIDATE_EMAIL)) {
+        if (filter_var($event->user->contact, FILTER_VALIDATE_EMAIL)) {
             Mail::to($event->user->contact)->send(new WelcomeMail($event->user));
         }
 
