8789372dfc816255262b156c64938e56d0055c83
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-04-18 21:03:14 +0200

    Refactor Doctor model to include HasFactory trait and add DoctorFactory for model testing

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 use App\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 
 class Doctor extends Model
 {
-    use LogsActivity , Notifiable;
+    use LogsActivity , Notifiable, HasFactory;
 
     protected $fillable = [
         'user_id',
