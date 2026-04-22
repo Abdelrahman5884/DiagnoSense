1ae10280ad4bc1ffc644f4461614d658095c0475
Menna-Baligh <mennabaligh55520@yahoo.com>
2026-04-22 03:31:46 +0200

    refactor(doctor): move profile update logic to DoctorService with DB transactions

---
+++ app/Services/DoctorService.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Services;
+
+use App\Models\Doctor;
+use Illuminate\Support\Facades\DB;
+
+class DoctorService
+{
+    public function updateProfile(Doctor $doctor, array $data): Doctor
+    {
+        return DB::transaction(function () use ($doctor, $data) {
+            $doctor->user()->update([
+                'name' => $data['name']
+            ]);
+
+            if (isset($data['specialization'])) {
+                $doctor->update([
+                    'specialization' => $data['specialization']
+                ]);
+            }
+
+            return $doctor->load('user');
+        });
+    }
+
+}
