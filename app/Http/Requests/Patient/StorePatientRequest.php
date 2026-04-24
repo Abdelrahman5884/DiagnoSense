110610be48bd1ef5353c36cc458b8383af4fe14a
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-04-24 21:57:19 +0300

    Add PatientListRequest and StorePatientRequest with validation rules and messages

--- app/Http/Requests/Patient/StorePatientRequest.php
+++ app/Http/Requests/Patient/StorePatientRequest.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Patient;
 
+use App\Rules\ValidContactRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -16,33 +17,20 @@ class StorePatientRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => [
-                'required_without:phone',
-                'string',
-                'email',
-                'max:255',
-                Rule::unique('users')->where(function ($query) {
-                    return $query->where('type', 'patient');
-                }),
+            'contact' => [
+                'required',
+                new ValidContactRule,
+                'bail',
+                Rule::unique('users', 'contact'),
             ],
-            'phone' => [
-                'required_without:email',
-                'string',
-                'min:10',
-                'max:15',
-                Rule::unique('users')->where(function ($query) {
-                    return $query->where('type', 'patient');
-                }),
-            ],
-            'age' => ['required', 'integer'],
+            'date_of_birth' => ['required', 'date'],
             'gender' => ['required', 'string', 'in:male,female'],
-            'national_id' => ['nullable', 'string', 'max:15', 'unique:patients,national_id'],
+            'national_id' => ['nullable', 'string', 'max:14', 'unique:patients,national_id'],
             'is_smoker' => ['nullable', 'boolean'],
-            'previous_surgeries' => ['nullable', 'boolean'],
             'chronic_diseases' => ['nullable', 'array'],
             'chronic_diseases.*' => ['string'],
-            'previous_surgeries_name' => ['required_if:previous_surgeries,true', 'prohibited_if:previous_surgeries,false', 'string'],
-            'medications' => ['nullable', 'string'],
+            'previous_surgeries_name' => ['nullable', 'string'],
+            'current_medications' => ['nullable', 'string'],
             'allergies' => ['nullable', 'string'],
             'family_history' => ['nullable', 'string'],
             'lab' => ['required_without_all:radiology,medical_history', 'array'],
@@ -58,17 +46,14 @@ class StorePatientRequest extends FormRequest
     public function messages()
     {
         return [
-            'email.unique' => 'Email already exists.',
-            'phone.unique' => 'Phone number already exists.',
-            'national_id.unique' => 'National ID already exists.',
+            'contact.required' => 'The contact field is required.',
+            'contact.unique' => 'The contact has already been taken.',
             'lab.required_without_all' => 'Please upload at least one lab test result or radiology report or medical history report.',
             'radiology.required_without_all' => 'Please upload at least one lab test result or radiology report or medical history report.',
             'medical_history.required_without_all' => 'Please upload at least one lab test result or radiology report or medical history report.',
-            'lab.*.mimes' => 'Each lab file must be a PDF document.',
-            'radiology.*.mimes' => 'Each radiology report must be a PDF document.',
-            'medical_history.*.mimes' => 'Each medical history file must be a PDF document.',
-            'previous_surgeries_name.required_if' => 'Please provide the names of previous surgeries.',
-            'previous_surgeries_name.prohibited_if' => 'Previous surgeries name should not be provided.',
+            'lab.*.mimes' => 'Each lab test must be a PDF document Or jpg image.',
+            'radiology.*.mimes' => 'Each radiology report must be a PDF document Or jpg image',
+            'medical_history.*.mimes' => 'Each medical history file must be a PDF document Or jpg image.',
         ];
     }
 }
