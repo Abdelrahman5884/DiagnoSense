1b8cdc4d229fae8e0566bcac4b00849014e85a42
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-04-25 18:13:50 +0300

    Add factories for AiAnalysisResult and MedicalHistory; fix typos and add HasFactory to AiAnalysisResult model

--- app/Models/AiAnalysisResult.php
+++ app/Models/AiAnalysisResult.php
@@ -3,11 +3,12 @@
 namespace App\Models;
 
 use App\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AiAnalysisResult extends Model
 {
-    // use LogsActivity;
+    use HasFactory;
 
     protected $fillable = [
         'patient_id',
