e8877f4fe4d40574ce1e62401357636bf1852772
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-04-26 15:47:17 +0300

    add MarkNotificationAsReadRequest to validate notification read requests

---
+++ app/Http/Requests/MarkNotificationAsReadRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class MarkNotificationAsReadRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        $notification = $this->route('notification');
+        return $this->user()->doctor->notifications()->where('id', $notification->id)->exists();
+    }
+
+}
