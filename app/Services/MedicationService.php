dc259b1f3114f43b766380948948e17fbf0095bd
Menna-Baligh <mennabaligh55520@yahoo.com>
2026-05-23 14:07:09 +0300

    feat(service): implement getPatientMedications to fetch sorted prescription records

--- app/Services/MedicationService.php
+++ app/Services/MedicationService.php
@@ -4,10 +4,18 @@ namespace App\Services;
 
 use App\Actions\Visit\StoreMedicationAction;
 use App\Models\Medication;
+use App\Models\Patient;
 use App\Models\Visit;
+use Illuminate\Support\Collection;
 
 class MedicationService
 {
+    public function getPatientMedications(Patient $patient): Collection
+    {
+        return $patient->medications()
+            ->latest()
+            ->get();
+    }
     public function store(Visit $visit, array $data): Medication
     {
         return (new StoreMedicationAction)->execute($visit, $data);
