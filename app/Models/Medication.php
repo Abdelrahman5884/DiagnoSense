788dbc80744022420437b47538070d6ec4e3c8a8
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-05-24 21:38:44 +0300

    Type-hint `patient()`, `doctor()`, and `visit()` methods in `Medication` model with `BelongsTo`

--- app/Models/Medication.php
+++ app/Models/Medication.php
@@ -5,18 +5,11 @@ namespace App\Models;
 use App\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Medication extends Model
 {
-    use HasFactory;
-    // use LogsActivity;
-
-    protected array $logOnlyEvents = ['created', 'updated', 'deleted'];
-
-    public function toActivityDisplayName(): string
-    {
-        return "Medication: '{$this->name}'";
-    }
+    use HasFactory, LogsActivity;
 
     protected $fillable = [
         'name',
@@ -26,18 +19,26 @@ class Medication extends Model
         'duration',
         'visit_id',
     ];
+    protected array $logOnlyEvents = ['created', 'updated', 'deleted'];
 
-    public function patient()
+    public function toActivityDisplayName(): string
+    {
+        return "Medication: '{$this->name}'";
+    }
+
+
+    public function patient(): BelongsTo
     {
         return $this->belongsTo(Patient::class);
     }
 
-    public function doctor()
+    public function doctor(): BelongsTo
     {
         return $this->belongsTo(Doctor::class);
     }
 
-    public function visit()
+    public function visit(): BelongsTo
+
     {
         return $this->belongsTo(Visit::class);
     }
