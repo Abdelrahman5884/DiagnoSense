b1b85292e36bcd2c32811fb1758edfb9923ae9be
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-05-24 22:16:40 +0300

    Remove unused seeders: `PatientSeeder`, `AiAnalysisSeeder`, and `SearchSeeder`, along with their references in `DatabaseSeeder`

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,8 +23,6 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             PlanSeeder::class,
-            PatientSeeder::class,
-            AiAnalysisSeeder::class,
         ]);
 
     }
