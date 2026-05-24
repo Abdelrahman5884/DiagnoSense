8a93c43f9e7123490ec441f79394819f91bfda0b
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-05-24 20:24:43 +0300

    Add explicit JsonResponse return type to `render` method in HMAC exceptions

--- app/Exceptions/InvalidHmacException.php
+++ app/Exceptions/InvalidHmacException.php
@@ -3,11 +3,12 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class InvalidHmacException extends Exception
 {
-    public function render(Request $request)
+    public function render(Request $request): JsonResponse
     {
         return response()->json(['error' => 'Invalid HMAC'], 401);
     }
--- app/Exceptions/MissingHmacException.php
+++ app/Exceptions/MissingHmacException.php
@@ -3,11 +3,12 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MissingHmacException extends Exception
 {
-    public function render(Request $request)
+    public function render(Request $request): JsonResponse
     {
         return response()->json(['error' => 'Missing HMAC'], 400);
     }
