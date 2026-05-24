be7b6c535081aa859db94b4b47a7903f1ea4b87d
Hagar Elbakry <hagarelbakry12@gmail.com>
2026-05-24 21:34:02 +0300

    Type-hint `patient()` and related methods in `AiAnalysisResult` model with `BelongsTo` and `HasMany`

--- app/Models/AiAnalysisResult.php
+++ app/Models/AiAnalysisResult.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AiAnalysisResult extends Model
 {
@@ -22,17 +24,17 @@ class AiAnalysisResult extends Model
         'response' => 'array',
     ];
 
-    public function patient()
+    public function patient(): BelongsTo
     {
         return $this->belongsTo(Patient::class);
     }
 
-    public function keyPoints()
+    public function keyPoints(): HasMany
     {
         return $this->hasMany(KeyPoint::class);
     }
 
-    public function decisionSupports()
+    public function decisionSupports(): HasMany
     {
         return $this->hasMany(DecisionSupport::class);
     }
